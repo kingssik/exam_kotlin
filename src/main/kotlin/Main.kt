e3cddc44439c7f2edab746d7f4d7207572b68217
kingssik <hyunsikkang25@gmail.com>
2023-07-27 00:41:45 +0900

    when 문

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,10 @@ fun main(args: Array<String>) {
 
     val price: Int = 100
 
-    if (price in arrayOf(100, 200, 300)) {
-        println("contain")
-    } else {
-        println("not contained")
+    when (price) {
+        100 -> println("1. price = $price") // price 값이 100일 경우 실행
+        200 -> println("2. price = $price") // price 값이 200일 경우 실행
+        300 -> println("3. price = $price") // price 값이 300일 경우 실행
+        else -> println("4. Not")    // 조건에 해당하는 price 값이 없을 경우 실행
     }
 }
