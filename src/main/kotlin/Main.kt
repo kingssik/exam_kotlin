dd9de126743f5c45676ddcf1ce39edfd4868beab
kingssik <hyunsikkang25@gmail.com>
2023-07-27 00:26:10 +0900

    if문

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,11 @@
 fun main(args: Array<String>) {
 
-    // 타입 추론 : kotlin은 타입추론으로 변수에 들어오는 값을 보고 타입을 알아서 지정해줌
-    val s = "ABC"
-    val i = 1
-    val l = 1L
-    val d = 1.0
-    val f = 1.0f
+    val priceA = 100
+    val priceB = 200
 
-    println("s = " + s::class)  // s = class java.lang.String (Kotlin reflection is not available)
-    println("i = " + i::class)  // i = int (Kotlin reflection is not available)
-    println("l = " + l::class)  // l = long (Kotlin reflection is not available)
-    println("d = " + d::class)  // d = double (Kotlin reflection is not available)
-    println("f = " + f::class)  // f = float (Kotlin reflection is not available)
+    if (priceA >= priceB) {
+        println("priceA = $priceA") // 조건식 true일 경우 실행
+    } else {
+        println("priceB = $priceB") // 조건식 false일 경우 실행
+    }
 }
