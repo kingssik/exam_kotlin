072e71c75dad003850b7cfd33f4bfb780ebdc509
kingssik <hyunsikkang25@gmail.com>
2023-07-27 00:48:45 +0900

    when 문 - 범위 비교

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,9 @@ fun main(args: Array<String>) {
     val price: Int = 100
 
     when (price) {
-        100 -> println("1. price = $price") // price 값이 100일 경우 실행
-        200 -> println("2. price = $price") // price 값이 200일 경우 실행
-        300 -> println("3. price = $price") // price 값이 300일 경우 실행
-        else -> println("4. Not")    // 조건에 해당하는 price 값이 없을 경우 실행
+        in 100..199 -> println("1. price = $price") // price 값이 해당 범위 내에 있을 경우 실행
+        in 200..299 -> println("2. price = $price") // price 값이 해당 범위 내에 있을 경우 실행
+        in 300..399 -> println("3. price = $price") // price 값이 해당 범위 내에 있을 경우 실행
+        else -> println("4. Not")    // 위 조건에 해당하는 경우가 없을 때 실행
     }
 }
