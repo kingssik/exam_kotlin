4149904ad66f8bff1ea74053fdad16ab3f124871
kingssik <hyunsikkang25@gmail.com>
2023-07-27 00:38:48 +0900

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,9 @@ fun main(args: Array<String>) {
 
     val price: Int = 100
 
-    if (price in arrayOf(100, 200, 300)) {
-        println("contain")
+    if (price in arrayOf(100, 200, 300)) {    
+        println("contain")    // price의 값이 100, 200, 300 중에 있을 경우 실행
     } else {
-        println("not contained")
+        println("not contained")     // price의 값이 100, 200, 300 중에 없을 경우 실행
     }
 }
