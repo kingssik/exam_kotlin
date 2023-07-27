f334009454a324e4aed5fb687df0dd85e6d73d99
kingssik <hyunsikkang25@gmail.com>
2023-07-28 01:03:22 +0900

    인터페이스

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,27 @@ fun main(args: Array<String>) {
 
     val item = Item("Book", 10_000)
     println("Item name is ${item.name}, price is ${item.price}")
+
+    item.buy()
+    item.sell()
 }
 
 // 클래스, 생성자 -> Item(var name: String, val price: Int)
 class Item(
     val name: String,
     val price: Int
-)
+) : ItemTrade {
+    override fun buy() {
+        println("[buy] $name")
+    }
 
+    override fun sell() {
+        println("[sell] $name")
+    }
+}
+
+// 인터페이스
+interface ItemTrade {
+    fun buy()
+    fun sell()
+}
