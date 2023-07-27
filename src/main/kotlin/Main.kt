904c58c5d964f0da97da46fe8943df87ad3bdccc
kingssik <hyunsikkang25@gmail.com>
2023-07-28 00:54:18 +0900

    클래스 - 생성자

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
 fun main(args: Array<String>) {
 
-    // 함수
-    val price1: Int = 100
-    val price2: Int = 200
-
-    val price3: Int = sumPrice(price1, price2)
-    println("price3 = $price3") // price3 = 300
+    val item = Item("Book", 10_000)
+    println("Item name is ${item.name}, price is ${item.price}")
 }
 
-fun sumPrice(price1: Int, price2: Int): Int {
-//    val price = price1 + price2
-    return price1 + price2  // 이런 경우 함수를 정의할 때 fun sumPrice(price1: Int, price2: Int) = price1 + price2 라고 쓸 수 있다
-}
+// 클래스, 생성자 -> Item(var name: String, val price: Int)
+class Item(
+    val name: String,
+    val price: Int
+)
+
