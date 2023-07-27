40e7d4a580292f5ba606723145935a538b07db05
kingssik <hyunsikkang25@gmail.com>
2023-07-28 00:47:23 +0900

    함수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 fun main(args: Array<String>) {
 
-    val price: Int = 100
+    // 함수
+    val price1: Int = 100
+    val price2: Int = 200
 
-    when (price) {
-        in 100..199 -> println("1. price = $price") // price 값이 해당 범위 내에 있을 경우 실행
-        in 200..299 -> println("2. price = $price") // price 값이 해당 범위 내에 있을 경우 실행
-        in 300..399 -> println("3. price = $price") // price 값이 해당 범위 내에 있을 경우 실행
-        else -> println("4. Not")    // 위 조건에 해당하는 경우가 없을 때 실행
-    }
+    val price3: Int = sumPrice(price1, price2)
+    println("price3 = $price3") // price3 = 300
+}
+
+fun sumPrice(price1: Int, price2: Int): Int {
+//    val price = price1 + price2
+    return price1 + price2  // 이런 경우 함수를 정의할 때 fun sumPrice(price1: Int, price2: Int) = price1 + price2 라고 쓸 수 있다
 }
